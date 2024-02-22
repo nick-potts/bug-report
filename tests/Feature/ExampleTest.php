e0089e5745929203b36039fbe5af344146f62eaa
Nick Potts <github@nickpotts.com.au>
2024-02-22 17:09:03 +0800

    update instructions

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,24 +1,42 @@
 <?php
 
-it('it fails dispatching in a loop', function () {
-
-
-    for ($i = 0; $i < 100000; $i++) {
-        \App\Jobs\BlankJob::dispatch();
-    }
-
-    expect(true)->toBeTrue();
-});
-
-
 it('succeeds with telescope recording disabled', function () {
-    \Laravel\Telescope\Telescope::stopRecording();
 
-    for ($i = 0; $i < 100000; $i++) {
+    //
+    ini_set('memory_limit', '50M');
+
+    \Laravel\Telescope\Telescope::stopRecording();
+    for ($i = 0; $i < 5000; $i++) {
         \App\Jobs\BlankJob::dispatch();
     }
-
     \Laravel\Telescope\Telescope::startRecording();
 
     expect(true)->toBeTrue();
 });
+
+it('it succeeds with 250mb', function () {
+
+    ini_set('memory_limit', '250M');
+
+    for ($i = 0; $i < 5000; $i++) {
+        \App\Jobs\BlankJob::dispatch();
+    }
+
+    expect(true)->toBeTrue();
+});
+
+
+it('it fails dispatching in a loop', function () {
+
+    ini_set('memory_limit', '50M');
+
+    for ($i = 0; $i < 5000; $i++) {
+        \App\Jobs\BlankJob::dispatch();
+    }
+
+    expect(true)->toBeTrue();
+});
+
+
+
+
