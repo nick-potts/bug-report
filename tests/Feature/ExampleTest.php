548bbc2cf4b22595af8f5eca09e6b2454fa61030
Nick Potts <github@nickpotts.com.au>
2024-02-22 16:15:38 +0800

    Init

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,24 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+it('it fails dispatching in a loop', function () {
 
-    $response->assertStatus(200);
+
+    for ($i = 0; $i < 100000; $i++) {
+        \App\Jobs\BlankJob::dispatch();
+    }
+
+    expect(true)->toBeTrue();
+});
+
+
+it('succeeds with telescope recording disabled', function () {
+    \Laravel\Telescope\Telescope::stopRecording();
+
+    for ($i = 0; $i < 100000; $i++) {
+        \App\Jobs\BlankJob::dispatch();
+    }
+
+    \Laravel\Telescope\Telescope::startRecording();
+
+    expect(true)->toBeTrue();
 });
