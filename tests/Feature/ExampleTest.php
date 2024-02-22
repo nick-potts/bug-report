ae2fc6e404733f23172a6e3d440523adfaca16ae
Nick Potts <github@nickpotts.com.au>
2024-02-22 18:15:21 +0800

    update tests to show where exactly it fails

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 it('succeeds with telescope recording disabled', function () {
 
-    //
     ini_set('memory_limit', '50M');
 
     \Laravel\Telescope\Telescope::stopRecording();
@@ -14,7 +13,7 @@ it('succeeds with telescope recording disabled', function () {
     expect(true)->toBeTrue();
 });
 
-it('it succeeds with 250mb', function () {
+it('succeeds with 250mb', function () {
 
     ini_set('memory_limit', '250M');
 
@@ -26,12 +25,25 @@ it('it succeeds with 250mb', function () {
 });
 
 
-it('it fails dispatching in a loop', function () {
+it('fails with recording enabled', function () {
 
     ini_set('memory_limit', '50M');
 
     for ($i = 0; $i < 5000; $i++) {
         \App\Jobs\BlankJob::dispatch();
+    }
+
+    expect(true)->toBeTrue();
+});
+
+it('fails because telescope entries', function () {
+
+    ini_set('memory_limit', '50M');
+
+    $content = ['key' => str_repeat('a', 100000)];
+
+    for ($i = 0; $i < 5000; $i++) {
+        \Laravel\Telescope\Telescope::recordJob(new \Laravel\Telescope\IncomingEntry($content, \Illuminate\Support\Str::uuid()));
     }
 
     expect(true)->toBeTrue();
